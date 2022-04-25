8f067220aa3ae5aad1bb531b5cb522946b293930
Garland Xie <garlandxie@gmail.com>
2022-04-25 10:23:07 -0400

    Initial commit

---
+++ scripts/master_script.R
@@ -0,0 +1,21 @@
+# libraries ----
+library(here)
+
+# processing scripts ----
+source(here("scripts", "processing_scripts", "01-summarize_trapnest_data.R"))
+source(here("scripts", "processing_scripts", "02-calculate_land_cover_metrics.R"))
+source(here("scripts", "processing_scripts", "03-clean_trait_data.R"))
+
+# analysis scripts -----
+source(here("scripts", "analysis_scripts", "01-crit1_func_alpha.R"))
+source(here("scripts", "analysis_scripts", "02-crit2_func_alpha.R"))
+source(here("scripts", "analysis_scripts", "03-crit3_RLQ.R"))
+
+# data appendix scripts ----
+source(here("scripts", "data_appendix_scripts", "figure_S1_and_S2.R"))
+source(here("scripts", "data_appendix_scripts", "figure_S3.R"))
+source(here("scripts", "data_appendix_scripts", "figure_S4.R"))
+
+
+
+
